9e32b2ddc20f3febaa586076d2c04f7d22afa28a
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2023-12-07 18:04:31 +0100

    main.py

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
 import discord
 from discord.ext import commands
-from keep_alive import keep_alive
-keep_alive()
 
-intents = discord.Intents.default()
-intents.message_content = True
-
+intents = discord.Intents.all()  # Enable all intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    print('------')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Ai"))
+    print(f"We have logged in as {bot.user}")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-@bot.command()
-async def roll(ctx):
-    import random
-    await ctx.send(f'You rolled a {random.randint(1, 6)}.')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
-
-@bot.command()
-async def kick(ctx, member: discord.Member):
-    owner_role = discord.utils.get(ctx.guild.roles, name="Owner")
-    if owner_role in ctx.author.roles:
-        await member.kick()
-        await ctx.send(f'{member} has been kicked.')
-    else:
-        await ctx.send("You don't have permission to use this command.")
-
-@bot.command()
-async def clear(ctx, amount: int):
-    owner_role = discord.utils.get(ctx.guild.roles, name="Owner")
-    if owner_role in ctx.author.roles:
-        await ctx.channel.purge(limit=amount+1)
-    else:
-        await ctx.send("You don't have permission to use this command.")
-
-bot.run('YOUR_BOT_TOKEN')
+bot.run('YOUR_BOT_TOKEN_HERE')  # Replace 'YOUR_BOT_TOKEN_HERE' with your bot's token
